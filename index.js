5acde599d85d9b143dcea6af64c3b2bde7626866
MastaCoder <makan@makan.bz>
2023-05-30 18:10:46 -0400

    chore: update node-pre-gyp

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-"use strict";
+'use strict';
 
 var gcEmitter,
-  binary = require('node-pre-gyp'),
+  binary = require('@mapbox/node-pre-gyp'),
   path = require('path'),
-  binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json'))),
+  binding_path = binary.find(
+    path.resolve(path.join(__dirname, './package.json'))
+  ),
   gcstats = require(binding_path),
   EventEmitter = require('events').EventEmitter;
 
 function gcStats() {
-  if (this instanceof gcStats){
-    throw Error('gc-stats no longer exports a constructor. Call without the `new` keyword');
+  if (this instanceof gcStats) {
+    throw Error(
+      'gc-stats no longer exports a constructor. Call without the `new` keyword'
+    );
   }
 
-  if(!gcEmitter) {
+  if (!gcEmitter) {
     gcEmitter = new EventEmitter();
-    gcstats.afterGC(function(stats) {
+    gcstats.afterGC(function (stats) {
       gcEmitter.emit('data', stats);
       gcEmitter.emit('stats', stats);
     });
